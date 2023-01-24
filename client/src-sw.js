a7a631552ff2de56503b6395730fbc455600b9a4
Tanner Kothlow <tannerkothlow@msn.com>
2023-01-24 12:41:25 -0600

    Cachefirst method works properly and install button downloads asset cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,19 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-// registerRoute(
-//   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-//   new CacheFirst({
-//     cacheName: 'asset-cache',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//       new ExpirationPlugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//       }),
-//     ],
-//   })
-// );
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
